3a4863fb3450218c344be67cddaf793a59a5bf41
annagi98 <44209794+annagi98@users.noreply.github.com>
2019-02-28 20:13:56 +0100

    Update Image.java

--- Image.java
+++ Image.java
@@ -3,12 +3,12 @@ import java.util.ArrayList;
 public class Image {
     ArrayList<String> tags;
     int numberOfTags;
-    boolean sizeOfImage;
+    int sizeOfImage;
     boolean isVertical;
     int idInFile;
     boolean isUsed;
 
-    public Image(ArrayList<String> tags, int numberOfTags, boolean sizeOfImage, boolean isVertical, int idInFile, boolean isUsed) {
+    public Image(ArrayList<String> tags, int numberOfTags, int sizeOfImage, boolean isVertical, int idInFile, boolean isUsed) {
         this.tags = tags;
         this.numberOfTags = numberOfTags;
         this.sizeOfImage = sizeOfImage;
