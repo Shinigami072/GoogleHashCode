21c31cefb1fcec7ea7f61045ae1e78ccdf8bbe3f
annagi98 <44209794+annagi98@users.noreply.github.com>
2019-02-28 19:44:10 +0100

    Update Image.java

--- Image.java
+++ Image.java
@@ -6,12 +6,14 @@ public class Image {
     boolean sizeOfImage;
     boolean isVertical;
     int idInFile;
+    boolean isUsed;
 
-    public Image(ArrayList<String> tags, int numberOfTags, boolean sizeOfImage, boolean isVertical, int idInFile) {
+    public Image(ArrayList<String> tags, int numberOfTags, boolean sizeOfImage, boolean isVertical, int idInFile, boolean isUsed) {
         this.tags = tags;
         this.numberOfTags = numberOfTags;
         this.sizeOfImage = sizeOfImage;
         this.isVertical = isVertical;
         this.idInFile = idInFile;
+        this.isUsed = isUsed;
     }
 }
