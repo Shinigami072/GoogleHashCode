5bdaa3f72dfeec8ddf0e5ac69f65d51ab1b495ca
Shinigami072 <krzys.stasiowski@gmail.com>
2019-02-28 21:15:51 +0100

    stuff

--- Main.java
+++ Main.java
@@ -1,8 +1,32 @@
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.Set;
 
 public class Main {
+
+    Slide nextSlide(PictureHolder hodler,Slide current){
+        Set<Image> tags = new HashSet<>();
+        for(String tag:current.sumOfTags)
+            tags.addAll(hodler.get(tag));
+
+        Image im = null;
+        int c = 0;
+        for(Image img:tags){
+            if(!img.isUsed && !img.isVertical)
+            {
+                int t =TagsComparator.imageCompare(current.sumOfTags,img.tags);
+                if(c<t)
+                {
+                    im = img;
+                    c=t;
+                }
+            }
+        }
+        im.isUsed=true;
+        return new Slide(im);
+    }
+
     public static void main(String... args) throws IOException {
         PictureHolder h = PictureHolder.make_Holder(args[0]);
         Set<Image> max_i= h.getLSet();
--- TagsComparator.java
+++ TagsComparator.java
@@ -1,29 +1,18 @@
 import java.util.*;
 
 public class TagsComparator {
-    Set<String> sameTags;
-    Set<String> in1NotIn2;
-    Set<String> in2NotIn1;
 
-    public TagsComparator(HashSet sameTags, HashSet isIn1butNotIn2, HashSet isIn2butNotIn1) {
-        this.sameTags = sameTags;
-        this.in1NotIn2 = in1NotIn2;
-        this.in2NotIn1 = in2NotIn1;
-    }
+    public static int imageCompare(Set<String> tags1, Set<String> tags2){
 
-    
-    public int imageCompare(Image img1, Image img2){
-        Set<String> tags1 = img1.tags;
-        Set<String> tags2 = img2.tags;
-        sameTags = new HashSet<>();
+        Set<String> sameTags = new HashSet<>();
         sameTags.addAll(tags1);
         sameTags.retainAll(tags2);
 
-        in1NotIn2 = new HashSet<>();
+        Set<String> in1NotIn2 = new HashSet<>();
         in1NotIn2.addAll(tags1);
         in1NotIn2.removeAll(tags2);
 
-        in2NotIn1 = new HashSet<>();
+        Set<String> in2NotIn1 = new HashSet<>();
         in2NotIn1.addAll(tags2);
         in2NotIn1.removeAll(tags1);
         
@@ -33,7 +22,7 @@ public class TagsComparator {
 
     }
 
-    public int minimumScore(Set<String> sameTags, Set<String> in1NotIn2, Set<String> in2NotIn1){
+    public static int minimumScore(Set<String> sameTags, Set<String> in1NotIn2, Set<String> in2NotIn1){
         List<Integer> listOfScores = new ArrayList<Integer>();
         listOfScores.add(sameTags.size());
         listOfScores.add(in1NotIn2.size());
